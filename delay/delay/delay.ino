4c66d773adde743ea1af10aa8faba01f74fb0dc6
David Quintas Viana <david.quintas.v13@gmail.com>
2024-11-27 10:24:48 +0100

    ejercicio2

---
+++ delay/delay/delay.ino
@@ -0,0 +1,16 @@
+int contador = 0;
+
+void setup() {
+  
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  
+  Serial.print("Contador: ");
+  Serial.println(contador);
+  contador++;
+  delay(1000);
+
+}
